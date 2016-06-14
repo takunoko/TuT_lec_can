6614941d0cc3d34868dd73d3a29180b9861e14ca
takunoko <takunoko@gmail.com>
2016-06-15 00:00:47 +0900

    削除ボタンの追加．(追加しただけで機能とか実装してない)

--- js/get_cancell_info.js
+++ js/get_cancell_info.js
@@ -8,6 +8,14 @@
 // とりあえずページが読み込まれた時点で実行
 // update_view();
 
+// 休講情報の番号を非表示ボタンに置き換える
+function add_hidden_button(tb_r){
+    console.log("ボタン追加");
+    // とりあえずボタンを追加した．
+    // cssいじったり，対応するJS書いたりする．
+    $("#grvCancel > tbody > tr > td:first-child").html("<button>削除</button>");
+}
+
 function update_view(){
     // debug
     var tb_c = document.getElementById('grvCancel');
@@ -37,10 +45,14 @@ function update_view(){
     // });
 
     // debug
+    // 削除ボタンの追加
+    add_hidden_button(tb_c_r);
+
+    // 要素の非表示について
     hidden_list = ["確率・統計論(確率・統計論-b)","ソフトウェア演習Ⅰ(ソフトウェア演習Ⅰ-a)"];
     // hidden_list = [];
 
-    my_data = conv_g_c( "B3","3");
+    my_data = conv_g_c( "","");
     com = false;
     hiddenUnnecessaryData(tb_c_r, my_data.grade, my_data.cls, com);
     hiddenUnnecessaryData(tb_s_r, my_data.grade, my_data.cls, com);
@@ -79,7 +91,7 @@ function convert_regexp(pattern_list){
     // パターンの文字列を正規表現で利用できるようにエスケープする
     // ()しか対応してないから事故るかも．．．
     regexp_str = pattern_list[0].replace(/\(/g, '\\(')
-                        .replace(/\)/g, '\\)');
+            .replace(/\)/g, '\\)');
     for (var s = 1; s < pattern_list.length; s++) {
         regexp_str += "|" + pattern_list[s]
             .replace(/\(/g, '\\(')
@@ -102,85 +114,79 @@ function subject_hidden(tb_r, hidden_list){
     hidden_str = convert_regexp(hidden_list);
 
     for (var i = 1, len = tb_r.length; i < len; i++) {
-        console.log("src : " + tb_r[i].cells[3].innerText);
-        console.log("match: " + hidden_str);
-        //        if (tb_r[i].cells[3].innerText.match(hidden_str) != null){
         if (tb_r[i].cells[3].innerText.match(hidden_str) != null){
-            console.log("まっちょ");
-            // ワンちゃん 必要?
             tb_r[i].style.display = 'none';
         }else{
+            // ワンちゃん 必要?
             // tb_r[i].style.display = '';
         }
     }
 
-    console.log(hidden_str);
+}
+
+// ハイライトの実行
+// state = 0:休講 | 1:補講
+function day_hilight(tb_r, state){
+    // 本日の日付のハイライト
+    today = new Date();
+    year = today.getFullYear();
+    month = today.getMonth()+1;
+    day = today.getDate();
+
+    if (parseInt(month).toString(10).length == 1){
+        month = "0" + month;
     }
+    if (parseInt(day).toString(10).length == 1){
+        day = "0" + day;
+    }
+    var date = (year + "/" + month + "/" + day);
 
-    // ハイライトの実行
-    // state = 0:休講 | 1:補講
-    function day_hilight(tb_r, state){
-        // 本日の日付のハイライト
-        today = new Date();
-        year = today.getFullYear();
-        month = today.getMonth()+1;
-        day = today.getDate();
-
-        if (parseInt(month).toString(10).length == 1){
-            month = "0" + month;
-        }
-        if (parseInt(day).toString(10).length == 1){
-            day = "0" + day;
-        }
-        var date = (year + "/" + month + "/" + day);
-
-        for (var i = 0, len = tb_r.length; i < len; i++) {
-            if (tb_r[i].cells[1].innerText.match(date)){
-                tb_r[i].style.backgroundColor = (state == 0) ? '#afeeee' : '#f93';
-            }
+    for (var i = 0, len = tb_r.length; i < len; i++) {
+        if (tb_r[i].cells[1].innerText.match(date)){
+            tb_r[i].style.backgroundColor = (state == 0) ? '#afeeee' : '#f93';
         }
     }
+}
 
-    // 検索文字列と選択を合わせる
-    function conv_g_c(g, c){
-        var gr, cl;
+// 検索文字列と選択を合わせる
+function conv_g_c(g, c){
+    var gr, cl;
 
-        // null = 未設定時
-        if (g != null){
-            if (g == 'all')
-                gr = '';
-            else
-                gr = g;
-        }else{
+    // null = 未設定時
+    if (g != null){
+        if (g == 'all')
             gr = '';
-        }
-
-        // クラスの判定
-        switch (c){
-            case 'all':
-                cl = '';
-                break;
-            case '1':
-                cl = '機械';
-                break;
-            case '2':
-                cl = '電気・電子情';
-                break;
-            case '3':
-                cl = '情報・知能';
-                break;
-            case '4':
-                cl = '環境・生命';
-                break;
-            case '5':
-                cl = '建築・都市';
-                break;
-            default:
-                // undefined(設定前なら)
-                cl = '';
-                break;
-        }
-
-        return {grade: gr, cls: cl}
+        else
+            gr = g;
+    }else{
+        gr = '';
     }
 
+    // クラスの判定
+    switch (c){
+        case 'all':
+            cl = '';
+            break;
+        case '1':
+            cl = '機械';
+            break;
+        case '2':
+            cl = '電気・電子情';
+            break;
+        case '3':
+            cl = '情報・知能';
+            break;
+        case '4':
+            cl = '環境・生命';
+            break;
+        case '5':
+            cl = '建築・都市';
+            break;
+        default:
+            // undefined(設定前なら)
+            cl = '';
+            break;
+    }
+
+    return {grade: gr, cls: cl}
+}
